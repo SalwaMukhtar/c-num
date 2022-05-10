13ba6219a9cff71286179abf8ccc4e0125bbe834
SalwaMukhtar <102853846+SalwaMukhtar@users.noreply.github.com>
2022-05-10 18:25:01 +0500

    Add files via upload

---
+++ abc3.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+
+
+int main() {
+   
+    int num;
+   
+ 
+   cout << "Hello world!"<<endl;
+    
+  
+   cout<<"enter any number:";
+  
+   cin>>num;
+  
+   cout<<"The number you entered is:"<<num;
+
+    
+
+
+      return 0;
+
+}
